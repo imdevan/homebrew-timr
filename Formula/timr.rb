cd64ae386d6f2acfd382318fe09872dcf1053b85
Devan Huapaya <huapayadevan@gmail.com>
2026-07-20 21:41:31 -0700

    Update timr to v0.4.0

--- Formula/timr.rb
+++ Formula/timr.rb
@@ -1,26 +1,26 @@
 class Timr < Formula
   desc "A simple cli timer"
   homepage "https://devan.gg/timr"
-  version "0.3.2"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-darwin-amd64.tar.gz"
-      sha256 "2f4650588b1127c18b47a073cb4a04b8f7a1f05df7ed07c9a0c0bd716236b97f"
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.4.0/timr-darwin-amd64.tar.gz"
+      sha256 "5f9569cf16c5bc21901ef3500d84958085dbd97c67303b1f4a9c55a2f9ef95d0"
     elsif Hardware::CPU.arm?
-      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-darwin-arm64.tar.gz"
-      sha256 "ebb06d951ea37c27c2d1f3b21d9b516eda471af75f267f445d74d062cdbdfbf8"
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.4.0/timr-darwin-arm64.tar.gz"
+      sha256 "dc0bbd95e7063466504ec0e8e12f69133ad5afc6078de69d426da86333e1e959"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-linux-amd64.tar.gz"
-      sha256 "db096c33b7b01be50a8ae465d82640ac166ee9885e95869b388be78add5c6fdf"
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.4.0/timr-linux-amd64.tar.gz"
+      sha256 "ed7bf675f51b0d0cf04b4a9e4faab2c9956232d8d02837f85150bd74b11ea997"
     elsif Hardware::CPU.arm?
-      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-linux-arm64.tar.gz"
-      sha256 "0eabe77826ce1297425d64e25ad59750ac46a8f7287facaa745e91f87d126d18"
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.4.0/timr-linux-arm64.tar.gz"
+      sha256 "e4e504018c59365de839f0e1ab0be14dc6154544e1de47b556ca22dca53336f2"
     end
   end
 
@@ -31,6 +31,6 @@ class Timr < Formula
   end
 
   test do
-    assert_match "v0.3.2", shell_output("#{bin}/timr --version")
+    assert_match "v0.4.0", shell_output("#{bin}/timr --version")
   end
 end
