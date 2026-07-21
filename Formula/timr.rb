6f6202cd8da1c2d8873a847ff3868e08e91728b3
Devan Huapaya <huapayadevan@gmail.com>
2026-07-20 18:08:51 -0700

    Update timr to v0.3.2

--- Formula/timr.rb
+++ Formula/timr.rb
@@ -1,17 +1,36 @@
 class Timr < Formula
   desc "A simple cli timer"
   homepage "https://devan.gg/timr"
-  url "https://github.com/imdevan/timr-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  version "0.3.2"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-darwin-amd64.tar.gz"
+      sha256 "2f4650588b1127c18b47a073cb4a04b8f7a1f05df7ed07c9a0c0bd716236b97f"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-darwin-arm64.tar.gz"
+      sha256 "ebb06d951ea37c27c2d1f3b21d9b516eda471af75f267f445d74d062cdbdfbf8"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-linux-amd64.tar.gz"
+      sha256 "db096c33b7b01be50a8ae465d82640ac166ee9885e95869b388be78add5c6fdf"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/imdevan/timr-cli/releases/download/v0.3.2/timr-linux-arm64.tar.gz"
+      sha256 "0eabe77826ce1297425d64e25ad59750ac46a8f7287facaa745e91f87d126d18"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"timr"), "./cmd/timr"
+    binary = OS.mac? ? "timr-darwin-" : "timr-linux-"
+    binary += Hardware::CPU.intel? ? "amd64" : "arm64"
+    bin.install binary => "timr"
   end
 
   test do
-    assert_match "v0.1.0", shell_output("#{bin}/timr --version")
+    assert_match "v0.3.2", shell_output("#{bin}/timr --version")
   end
 end
